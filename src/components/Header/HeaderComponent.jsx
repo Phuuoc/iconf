7ea6a0dcf3112aae508c2618e0820b6fa1a7d58b
Phuuoc <phuocntgcc18019@fpt.edu.vn>
2023-04-04 19:13:22 +0700

    aa

---
+++ src/components/Header/HeaderComponent.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import "./header.css"
+const HeaderComponent = () => {
+  return (
+    <div id='header'>
+      <div className='text'>
+        <span>
+          logo
+        </span>
+      </div>
+    </div>
+  )
+}
+
+export default HeaderComponent;
--- src/page/Home/HomeScreen.jsx
+++ src/page/Home/HomeScreen.jsx
@@ -1,7 +1,10 @@
 import React from "react";
+import HeaderComponent from "../../components/Header/HeaderComponent";
 
 export default function HomeScreens() {
-    return ( 
-    <div>Home</div>
+    return (
+        <>
+            <HeaderComponent />
+        </>
     );
 }
